8abe3422014739736c38cc03598f95ff4d055852
Anu682-max <anulkhagvazaya5@gmail.com>
2025-07-29 18:36:30 +0900

    Импортын нэршил болон компонент файлын нэрийг case-sensitive болгож засав (Vercel-д зориулж)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Service from "./components/3.service/service";
-import Project from "./components/4.project/project";
-import CeoMessage from "./components/5.Ceo-message/ceo-message";
-import Contact from "./components/6.Contact/contact";
-import Footer from "./components/7.footer/footer";
-import Header from "./components/1.header/header";
-import Hero from "./components/2.hero/hero";
+import Service from "./components/3.service/Service";
+import Project from "./components/4.project/Project";
+import CeoMessage from "./components/5.Ceo-message/Ceo-message";
+import Contact from "./components/6.Contact/Contact";
+import Footer from "./components/7.footer/Footer";
+import Header from "./components/1.header/Header";
+import Hero from "./components/2.Hero/Hero";
 function App() {
   return (
     <>
