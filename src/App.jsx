2e8ba91c9cf43aac2b7c03e01f5075f389186c50
Baljir0901 <baljir0901@gmail.com>
2025-07-29 19:34:14 +0900

    fix: correct import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Service from "./components/3.service/Service";
-import Project from "./components/4.project/Project";
-import CeoMessage from "./components/5.ceo-message/ceo-message";
-import Contact from "./components/6.contact/Contact";
-import Footer from "./components/7.footer/Footer";
-import Header from "./components/1.header/header";
-import Hero from "./components/2.hero/Hero";
+import Service from "./components/3.service/service";
+import Project from "./components/4.project/project";
+import CeoMessage from "./components/5.Ceo-message/ceo-message";
+import Contact from "./components/6.Contact/contact";
+import Footer from "./components/7.footer/footer";
+import Header from "./components/1.header/Header";
+import Hero from "./components/2.Hero/hero";
 function App() {
   return (
     <>
